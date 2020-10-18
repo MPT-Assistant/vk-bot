c1fa0439362843b4e36c6170c99c40c5a3cfc39b
rus_anonym <alexandrsemin2033@gmail.com>
2020-10-18 06:04:07 +0300

    add eval command

---
+++ src/commands/eval.ts
@@ -0,0 +1,23 @@
+import { MessageContext } from "vk-io";
+export = {
+	regexp: /^(?:zz)\s([^]+)$/i,
+	process: async (message: MessageContext) => {
+		if (message.senderId !== 266982306) {
+			return;
+		}
+		try {
+			const v = await eval(message.args[1]);
+			if (typeof v === "string") {
+				return await message.send(`Результат: ${v}`);
+			} else if (typeof v === "number") {
+				return await message.send(`Значение: ${v}`);
+			} else {
+				return await message.send(
+					`JSON Stringify: ${JSON.stringify(v, null, "　\t")}`,
+				);
+			}
+		} catch (err) {
+			return await message.send(`Ошибка: ${err.toString()}`);
+		}
+	},
+};
