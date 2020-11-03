a2f5617edd80cf08fd692dbb1b74faa60bedb06c
rus_anonym <alexandrsemin2033@gmail.com>
2020-11-04 02:33:12 +0300

    add save userGoogleAccount

--- src/commands/google/getToken.ts
+++ src/commands/google/getToken.ts
@@ -1,6 +1,7 @@
 import { MPTMessage } from "../../plugins/types";
 import { vk } from "../../plugins/core";
 import { google } from "../../plugins/google";
+import models from "../../plugins/models";
 export = {
 	regexp: /^(?:привязка)$/i,
 	process: async (message: MPTMessage) => {
@@ -8,17 +9,32 @@ export = {
 			return message.sendMessage(`команда доступна только в ЛС бота.`);
 		} else {
 			await message.sendMessage(
-				`для привязки аккаунта Google к боту, получите токен по ссылке: ${await vk.api.utils.getShortLink(
-					{ url: await google.getURLtoGetToken() },
-				)} и отправьте его боту.`,
+				`для привязки аккаунта Google к боту, получите токен по ссылке: ${
+					(
+						await vk.api.utils.getShortLink({
+							url: await google.getURLtoGetToken(),
+						})
+					).short_url
+				} и отправьте его боту.`,
 			);
-			let token = await message.question(`Токен аккаунта:`);
+			let token = await message.question(`Введите токен:`);
 			await message.sendMessage(`проверяю токен...`);
 			try {
 				if (token.text) {
 					let userData = await google.getUserTokenByTempToken(token.text);
-					// Доделать потом
-					return await message.sendMessage(`токен указан верно.`);
+					let userGoogleAccount: any = await models.userGoogle.findOne({
+						vk_id: message.senderId,
+					});
+					if (!userGoogleAccount) {
+						userGoogleAccount = new models.userGoogle({
+							vk_id: message.senderId,
+							token: userData,
+						});
+					} else {
+						userGoogleAccount.token = userData;
+					}
+					await message.sendMessage(`токен указан верно.`);
+					return await userGoogleAccount.save();
 				} else {
 					return await message.sendMessage(`неверно указан токен.`);
 				}
