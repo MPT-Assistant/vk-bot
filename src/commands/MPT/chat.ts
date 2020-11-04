cb5394566ef386c17c189de87ab7140967105d02
rus_anonym <alexandrsemin2033@gmail.com>
2020-11-04 17:34:20 +0300

    fix bug #3

--- src/commands/MPT/chat.ts
+++ src/commands/MPT/chat.ts
@@ -12,27 +12,33 @@ export = {
 		let groupData: any = await models.utilityGroup.findOne({
 			uid: message.chat.unical_group_id,
 		});
-		let keyboardData = [
-			[
-				Keyboard.textButton({
-					label: `${
-						message.chat.inform === true ? "Отключить" : "Включить"
-					} рассылку изменений`,
-					payload: {
-						command: `изменения ${
-							message.chat.inform === true ? "выкл" : "вкл"
-						}`,
-					},
-					color: message.chat.inform
-						? Keyboard.NEGATIVE_COLOR
-						: Keyboard.POSITIVE_COLOR,
-				}),
-			],
-		];
+		let groupText = ``;
+		let keyboardData: any = [];
+		if (groupData) {
+			groupText = `Привязан к группе: ${groupData.name}`;
+			keyboardData = [
+				[
+					Keyboard.textButton({
+						label: `${
+							message.chat.inform === true ? "Отключить" : "Включить"
+						} рассылку изменений`,
+						payload: {
+							command: `изменения ${
+								message.chat.inform === true ? "выкл" : "вкл"
+							}`,
+						},
+						color: message.chat.inform
+							? Keyboard.NEGATIVE_COLOR
+							: Keyboard.POSITIVE_COLOR,
+					}),
+				],
+			];
+		}
+
 		return message.sendMessage(
 			`информация о чате:
 ID: ${message.chat.id}
-Привязан к группе: ${groupData.name || "не привязан"}
+${groupText}
 Информирование о заменах: ${
 				message.chat.inform === true ? `Включено` : `Отключено`
 			}`,
