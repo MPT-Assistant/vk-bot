13cd1dcee45a5e9e6c136f3202cf1ad6e8d4ced4
rus_anonym <alexandrsemin2033@gmail.com>
2020-11-15 21:57:51 +0300

    add informReplacement to private messages

--- src/commands/MPT/informReplacements.ts
+++ src/commands/MPT/informReplacements.ts
@@ -4,13 +4,10 @@ export = {
 	regexp: /^(?:изменения)\s(вкл|выкл|выключить|включить)$/i,
 	template: [],
 	process: async (message: MPTMessage) => {
-		if (!message.chat) {
-			return message.sendMessage(
-				`команду можно использовать только в беседах.`,
-			);
-		}
 		if (/вкл|включить/gi.test(message.args[1]) === true) {
-			message.chat.inform = true;
+			message.chat
+				? (message.chat.inform = true)
+				: (message.user.data.replacement_notices = true);
 			return await message.sendMessage(
 				`рассылка изменений в расписании группы включена!`,
 				{
@@ -30,7 +27,9 @@ export = {
 		}
 
 		if (/выкл|выключить/gi.test(message.args[1]) === true) {
-			message.chat.inform = false;
+			message.chat
+				? (message.chat.inform = false)
+				: (message.user.data.replacement_notices = false);
 			return await message.sendMessage(
 				`рассылка изменений в расписании группы отключена!`,
 				{
