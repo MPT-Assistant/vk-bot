1bd75c03b17fa67095ffaf8920b0a6a7cdae6f35
rus_anonym <alexandrsemin2033@gmail.com>
2020-11-20 16:39:06 +0300

    add google.classroom.courses

--- src/commands/google/classroom/courses.ts
+++ src/commands/google/classroom/courses.ts
@@ -1,3 +1,4 @@
+import { utils } from "rus-anonym-utils";
 import { Keyboard } from "vk-io";
 import { MPTMessage } from "../../../plugins/types";
 import models from "../../../plugins/models";
@@ -34,25 +35,63 @@ export = {
 			//@ts-ignore
 			let classroomInstance = new classroomUser(userGoogleAccount.token);
 			let userCourses = await classroomInstance.courses.list();
+			if (!userCourses) {
+				return message.sendMessage(`у Вас не найдено курсов.`);
+			}
 			const pagesBuilder = message.pageBuilder();
-			pagesBuilder.setPages(["1 страница", "2 страница"]).build();
-			return message.sendMessage(
-				`Ваш профиль:
-Account: ${userGoogleAccount.email}`,
-				{
-					keyboard: Keyboard.keyboard([
-						[
-							Keyboard.textButton({
-								label: "Мои курсы",
-								payload: {
-									command: `Мои курсы`,
-								},
-								color: Keyboard.POSITIVE_COLOR,
-							}),
-						],
-					]).inline(),
-				},
-			);
+			pagesBuilder.setInfinityLoop(false);
+			let pagesArray = [];
+			let forwardData: any = {};
+			if (message.isChat) {
+				forwardData.forward = JSON.stringify({
+					peer_id: message.peerId,
+					conversation_message_ids: message.conversationMessageId,
+					is_reply: 1,
+				});
+			} else {
+				forwardData.reply_to = message.id;
+			}
+			for (let course of userCourses) {
+				pagesArray.push(
+					Object.assign(
+						{
+							message: `Название: ${
+								course.name
+							}\nСоздан: ${utils.time.getDateTimeByMS(
+								Number(new Date(course.creationTime || "")),
+							)}\nОбновлён: ${utils.time.getDateTimeByMS(
+								Number(new Date(course.updateTime || "")),
+							)}\nСсылка на курс: ${course.alternateLink}\nStatus: ${
+								course.courseState
+							}`,
+							dont_parse_link: 1,
+						},
+						forwardData,
+					),
+				);
+			}
+			pagesBuilder.setPages(pagesArray);
+			const keyboard = pagesBuilder.keyboard;
+			keyboard
+				.textButton({
+					label: "Кнопка с номером страницы",
+					payload: {
+						builder_id: pagesBuilder.id,
+						builder_page: 2,
+					},
+				})
+				.row()
+				.textButton({
+					label: "Кнопка с действием",
+					payload: {
+						builder_id: pagesBuilder.id,
+						builder_action: "next",
+					},
+				});
+			pagesBuilder.updateKeyboard(keyboard);
+			pagesBuilder.build();
+
+			return;
 		}
 	},
 };
