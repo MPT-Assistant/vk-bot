8c04b7b21141ad2de39f4dc283aa588f899a64d2
rus_anonym <alexandrsemin2033@gmail.com>
2020-11-20 15:06:55 +0300

    add google.profile

--- src/commands/google/profile.ts
+++ src/commands/google/profile.ts
@@ -10,6 +10,44 @@ export = {
 		if (message.isChat) {
 			return message.sendMessage(`команда доступна только в ЛС бота.`);
 		} else {
+			let userGoogleAccount = await models.userGoogle.findOne({
+				vk_id: message.senderId,
+			});
+			if (!userGoogleAccount) {
+				return message.sendMessage(
+					`Вы ещё не привязали свой аккаунт Google к боту.`,
+					{
+						keyboard: Keyboard.keyboard([
+							[
+								Keyboard.textButton({
+									label: "Привязать аккаунт",
+									payload: {
+										command: `Привязка`,
+									},
+									color: Keyboard.POSITIVE_COLOR,
+								}),
+							],
+						]).inline(),
+					},
+				);
+			}
+			return message.sendMessage(
+				`Ваш профиль:
+Account: ${userGoogleAccount.email}`,
+				{
+					keyboard: Keyboard.keyboard([
+						[
+							Keyboard.textButton({
+								label: "Classroom",
+								payload: {
+									command: `Classroom`,
+								},
+								color: Keyboard.POSITIVE_COLOR,
+							}),
+						],
+					]).inline(),
+				},
+			);
 		}
 	},
 };
