44ab10b7529ae399442b13c51fdcf366ad474c11
rus_anonym <alexandrsemin2033@gmail.com>
2021-04-02 00:19:36 +0300

    update logger

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -1,10 +1,11 @@
-import { getRandomId } from "vk-io";
+import { VK, getRandomId } from "vk-io";
 import { MessagesSendParams } from "vk-io/lib/api/schemas/params";
-import vk from "../vk";
+import config from "../../DB/config.json";
 
 class Logger {
+	private vk = new VK({ token: config.vk.logger.token });
 	public sendLog(text: string, params: MessagesSendParams = {}) {
-		return vk.api.messages.send(
+		return this.vk.api.messages.send(
 			Object.assign(
 				{ message: text, chat_id: 1, random_id: getRandomId() },
 				params,
