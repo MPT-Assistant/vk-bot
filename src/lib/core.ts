c317b77825acedc5a4ed0d59406a63ba688ca848
rus_anonym <alexandrsemin2033@gmail.com>
2021-04-02 19:02:11 +0300

    on start getLastDump

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -6,5 +6,6 @@ import vk from "./vk";
 new Interval(async () => {
 	await InternalUtils.mpt.getLastDump();
 }, 30000);
+InternalUtils.mpt.getLastDump();
 
 vk.updates.startPolling().then(() => console.log("Polling started"));
