c684270f221427cac30d2e1227157f50cb9c26f8
RusAnonym <alexandrsemin2033@gmail.com>
2021-04-04 01:21:16 +0300

    add nickname command

---
+++ src/commands/utils/nickname.ts
@@ -0,0 +1,9 @@
+import Command from "../../lib/utils/classes/command";
+
+new Command(/ник(\s(.*))?$/i, ["Ник"], async (message) => {
+	if (message.args[1].length > 25) {
+		return message.sendMessage(`максимальная длина ника 25 символов.`);
+	}
+	message.user.data.nickname = message.args[1];
+	return message.sendMessage(`Вы успешно сменили ник.`);
+});
