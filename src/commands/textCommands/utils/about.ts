a3b2973036b47a4ed692a37fa37302ca866f3a99
RusAnonym <alexandrsemin2033@gmail.com>
2021-04-04 01:42:38 +0300

    remove deprecated methods

--- src/commands/textCommands/utils/about.ts
+++ src/commands/textCommands/utils/about.ts
@@ -9,11 +9,11 @@ new Command(
 	async (message) => {
 		const API = await utils.vk.api.status();
 		let output = `Bot work already ${process.uptime()} sec\n`;
-		output += `Users: ${await InternalUtils.Bot_DB.models.user.count()}\n`;
-		output += `Chats: ${await InternalUtils.Bot_DB.models.chat.count()}\n`;
-		output += `Replacements: ${await InternalUtils.API_DB.models.replacement.count()}\n`;
-		output += `Specialties: ${await InternalUtils.API_DB.models.specialty.count()}\n`;
-		output += `Groups: ${await InternalUtils.API_DB.models.group.count()}\n\n`;
+		output += `Users: ${await InternalUtils.Bot_DB.models.user.countDocuments()}\n`;
+		output += `Chats: ${await InternalUtils.Bot_DB.models.chat.countDocuments()}\n`;
+		output += `Replacements: ${await InternalUtils.API_DB.models.replacement.countDocuments()}\n`;
+		output += `Specialties: ${await InternalUtils.API_DB.models.specialty.countDocuments()}\n`;
+		output += `Groups: ${await InternalUtils.API_DB.models.group.countDocuments()}\n\n`;
 
 		output += `VK API Status:\n`;
 		for (let i in API) {
