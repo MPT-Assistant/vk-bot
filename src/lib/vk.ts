fe5e235fca6870d607ff8ad65f7e5c575f02e072
RusAnonym <alexandrsemin2033@gmail.com>
2021-04-04 14:16:14 +0300

    final

--- src/lib/vk.ts
+++ src/lib/vk.ts
@@ -39,12 +39,7 @@ vk.updates.on(
 		if (context.messagePayload) {
 			context.text = context.messagePayload.command;
 		}
-		if (
-			context.isOutbox ||
-			context.isGroup ||
-			!context.text ||
-			context.senderId !== 266982306
-		) {
+		if (context.isOutbox || context.isGroup || !context.text) {
 			return;
 		}
 		context.text = context.text
