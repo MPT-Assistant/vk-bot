fa878932a6e2cf8837fe9d40e19214bffe28514b
RusAnonym <alexandrsemin2033@gmail.com>
2021-04-08 11:11:22 +0300

    add error handler on mailing

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -22,41 +22,45 @@ new Interval(async () => {
 		usersNotInformed.map(async (user) => {
 			user.reported_replacements.push(replacement.hash);
 			user.markModified("reported_replacements");
-			await user.save();
 			const replacementDate = moment(replacement.date).format("DD.MM.YYYY");
-			await vk.api.messages.send({
-				peer_id: user.id,
-				random_id: getRandomId(),
-				message: `Обнаружена новая замена на ${replacementDate}
+			try {
+				await vk.api.messages.send({
+					peer_id: user.id,
+					random_id: getRandomId(),
+					message: `Обнаружена новая замена на ${replacementDate}
 Пара: ${replacement.lessonNum}
 Заменяемая пара: ${replacement.oldLessonName}
 Преподаватель: ${replacement.oldLessonTeacher}
 Новая пара: ${replacement.newLessonName}
 Преподаватель на новой паре: ${replacement.newLessonTeacher}
 Добавлена на сайт: ${moment(replacement.addToSite).format(
-					"HH:mm:ss | DD.MM.YYYY",
-				)}
+						"HH:mm:ss | DD.MM.YYYY",
+					)}
 Обнаружена ботом: ${moment(replacement.detected).format(
-					"HH:mm:ss | DD.MM.YYYY",
-				)}`,
-				keyboard: Keyboard.builder()
-					.inline()
-					.textButton({
-						label: "Расписание",
-						payload: {
-							command: `Расписание ${replacementDate}`,
-						},
-						color: Keyboard.SECONDARY_COLOR,
-					})
-					.row()
-					.textButton({
-						label: "Отключить рассылку",
-						payload: {
-							command: "Отключить рассылку",
-						},
-						color: Keyboard.NEGATIVE_COLOR,
-					}),
-			});
+						"HH:mm:ss | DD.MM.YYYY",
+					)}`,
+					keyboard: Keyboard.builder()
+						.inline()
+						.textButton({
+							label: "Расписание",
+							payload: {
+								command: `Расписание ${replacementDate}`,
+							},
+							color: Keyboard.SECONDARY_COLOR,
+						})
+						.row()
+						.textButton({
+							label: "Отключить рассылку",
+							payload: {
+								command: "Отключить рассылку",
+							},
+							color: Keyboard.NEGATIVE_COLOR,
+						}),
+				});
+			} catch (error) {
+				user.inform = false;
+			}
+			await user.save();
 		});
 		const chatsNotInformed = await InternalUtils.Bot_DB.models.chat.find({
 			group: replacement.group,
@@ -68,41 +72,46 @@ new Interval(async () => {
 		chatsNotInformed.map(async (chat) => {
 			chat.reported_replacements.push(replacement.hash);
 			chat.markModified("reported_replacements");
-			await chat.save();
 			const replacementDate = moment(replacement.date).format("DD.MM.YYYY");
-			await vk.api.messages.send({
-				chat_id: chat.id,
-				random_id: getRandomId(),
-				message: `Обнаружена новая замена на ${replacementDate}
+			try {
+				await vk.api.messages.send({
+					chat_id: chat.id,
+					random_id: getRandomId(),
+					message: `Обнаружена новая замена на ${replacementDate}
 Пара: ${replacement.lessonNum}
 Заменяемая пара: ${replacement.oldLessonName}
 Преподаватель: ${replacement.oldLessonTeacher}
 Новая пара: ${replacement.newLessonName}
 Преподаватель на новой паре: ${replacement.newLessonTeacher}
 Добавлена на сайт: ${moment(replacement.addToSite).format(
-					"HH:mm:ss | DD.MM.YYYY",
-				)}
+						"HH:mm:ss | DD.MM.YYYY",
+					)}
 Обнаружена ботом: ${moment(replacement.detected).format(
-					"HH:mm:ss | DD.MM.YYYY",
-				)}`,
-				keyboard: Keyboard.builder()
-					.inline()
-					.textButton({
-						label: "Расписание",
-						payload: {
-							command: `Расписание ${replacementDate}`,
-						},
-						color: Keyboard.SECONDARY_COLOR,
-					})
-					.row()
-					.textButton({
-						label: "Отключить рассылку",
-						payload: {
-							command: "Отключить рассылку",
-						},
-						color: Keyboard.NEGATIVE_COLOR,
-					}),
-			});
+						"HH:mm:ss | DD.MM.YYYY",
+					)}`,
+					keyboard: Keyboard.builder()
+						.inline()
+						.textButton({
+							label: "Расписание",
+							payload: {
+								command: `Расписание ${replacementDate}`,
+							},
+							color: Keyboard.SECONDARY_COLOR,
+						})
+						.row()
+						.textButton({
+							label: "Отключить рассылку",
+							payload: {
+								command: "Отключить рассылку",
+							},
+							color: Keyboard.NEGATIVE_COLOR,
+						}),
+				});
+			} catch (error) {
+				chat.inform = false;
+			}
+
+			await chat.save();
 		});
 	}
 }, 5 * 60 * 1000);
